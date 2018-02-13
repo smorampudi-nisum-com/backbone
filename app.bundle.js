d941d09c8342ccf1b8ed2177eec7bf46dd78f7cd
santoshmai <30416641+smorampudi-nisum-com@users.noreply.github.com>
2018-02-13 12:06:24 -0800

    contact manager app with webpack

---
+++ app.bundle.js
@@ -0,0 +1,134 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+window.ContactManager = {
+  Models: {},
+  Collections: {},
+  Views: {},
+
+  start: function(data) {
+    var contacts = new ContactManager.Collections.Contacts(data.contacts),
+        router = new ContactManager.Router();
+
+    router.on('route:home', function() {
+      router.navigate('contacts', {
+        trigger: true,
+        replace: true
+      });
+    });
+
+    router.on('route:showContacts', function() {
+      var contactsView = new ContactManager.Views.Contacts({
+        collection: contacts
+      });
+
+      $('.main-container').html(contactsView.render().$el);
+    });
+
+    router.on('route:newContact', function() {
+      var newContactForm = new ContactManager.Views.ContactForm({
+        model: new ContactManager.Models.Contact()
+      });
+
+      newContactForm.on('form:submitted', function(attrs) {
+        attrs.id = contacts.isEmpty() ? 1 : (_.max(contacts.pluck('id')) + 1);
+        contacts.add(attrs);
+        router.navigate('contacts', true);
+      });
+
+      $('.main-container').html(newContactForm.render().$el);
+    });
+
+    router.on('route:editContact', function(id) {
+      var contact = contacts.get(id),
+          editContactForm;
+
+      if (contact) {
+        editContactForm = new ContactManager.Views.ContactForm({
+            model: contact
+        });
+
+        editContactForm.on('form:submitted', function(attrs) {
+          contact.set(attrs);
+          router.navigate('contacts', true);
+        });
+
+        $('.main-container').html(editContactForm.render().$el);
+      } else {
+        router.navigate('contacts', true);
+      }
+    });
+
+    Backbone.history.start();
+  }
+};
+
+
+/***/ })
+/******/ ]);
