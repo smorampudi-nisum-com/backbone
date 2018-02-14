bb34d802ec330df43ec026aa7c5e6da52cbf659e
santoshmai <30416641+smorampudi-nisum-com@users.noreply.github.com>
2018-02-13 16:22:42 -0800

    updated files of prev files

--- ContactManagerSpec.js
+++ ContactManagerSpec.js
@@ -3,14 +3,13 @@ describe('A page with backbone', function(){
         expect(window._).toBeDefined();
         expect(window._.VERSION).toBeDefined();
     });
-//     it('should have access to jquery', function(){
-//         expect(window.jQuery).toBeDefined();
-//         expect(window.jQuery.VERSION).toBeDefined();
-//     });
+    it('should have access to jquery', function(){
+        expect(window.jQuery).toBeDefined();
+    });
    it('should have access to Backbone', function(){
-    //expect(window.backbone).toBeDefined();
-    //expect(window.backbone.Model).toBeDefined();
-    //expect(window.backbone.Collection).toBeDefined();
-    //expect(window.backbone.View).toBeDefined();
+    expect(window.backbone).toBeDefined();
+    expect(window.backbone.Model).toBeDefined();
+    expect(window.backbone.Collection).toBeDefined();
+    expect(window.backbone.View).toBeDefined();
    });
 });
